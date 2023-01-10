674e6a9cfcf016ff833f1e2ed28b42ac4f7ff451
Agayeon <agy125@naver.com>
2023-01-10 09:53:38 +0900

    INIT : git init

--- numguess.py
+++ numguess.py
@@ -1,15 +1,11 @@
-from random import randint
+#Set username
+username = "John Doe"
+#print greeting with username
+print('Hello', username)
 
-# Generate answer
-answer= randint(1, 100)
-
-# print answer for debugging
-print(answer)
-
-#user interaction
-username=input("What's your name?")
-print("Hello, {}!!".format(username))
-
-guess = int(input("choose one number(1~100), {}!".format(username)))
-        print("{}!! you choose {}".format(username.guess))
-
+#Dont do
+animals = ['cat', 'dog','dragon']
+#DO
+menus = [ 'Ramen', 'Bear soup',
+        'Military soup, 
+        'Salad']
