1870fd411c3759b626a396b8a30a1358669d9610
gayeon <122242492+Agayeon@users.noreply.github.com>
2023-01-11 15:57:55 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
-import random
+from random import randint
+from time import sleep
+
 answer = random.randint(1, 100)
+
 #print answer for debugging
 print(answer)
-#username
-#Get and print 
-#User interation
+
 username = input("Hi, what your name?")
 print(f"Hi!!, {username}, choose answer")
 guess = int(input('choose number>'))
+print(f'Well choice {username}~~ You picked {guess}!!')
+
 if guess == answer:
-    print(f'you got it right The anser is {answer}) is 52!!')
+    print(f'You got it right!! The answer is {answer}!!')
 elif guess>answer:
     print(f,'keep going, man~! That was too high.. {username}...')
 elif guess<answer:
